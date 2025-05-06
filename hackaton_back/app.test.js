1682963b5e8884e742b78033a44c1dab34bbe020
treflest remy <mrtreflestremy@outlook.com>
2025-05-06 11:01:24 +0200

    feat(TU): backend 59% coverage + script command to run coverage

--- hackaton_back/app.test.js
+++ hackaton_back/app.test.js
@@ -1,36 +1,78 @@
 const request = require('supertest');
-const express = require('express');
 const app = require('./app'); // ou le chemin vers ton fichier app.js
+const nock = require('nock');
+require("dotenv").config();
+
+const base_url_communes = process.env.BDJSON ? "http://localhost:3001" : "https://geo.api.gouv.fr";
+const base_url_medecins = process.env.BDJSON ? "http://localhost:3001" : "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets";
+const base_url_localisation = "https://nominatim.openstreetmap.org";
 
 describe('API Express Tests', () => {
-  test('GET / doit retourner Hello World!', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World!');
-  });
 
-  test('GET /departements/73/communes doit retourner une liste de communes', async () => {
+  beforeEach(() => {
+    nock.cleanAll();
+  });
+  
+  test('GET /departements/73/communes doit retourner une liste de communes (mockée)', async () => {
+    nock(base_url_communes)
+      .get('/communes')
+      .reply(200, [
+        { nom: 'Albertville', code: '73011' },
+        { nom: 'Chambéry', code: '73065' }
+      ]);
+
     const res = await request(app).get('/departements/73/communes');
-    expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
-    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0].nom).toBe('Albertville');
   });
 
   test('GET /departements/73/medecins?codePostal=73000 retourne des médecins', async () => {
+    nock(base_url_medecins).get('/medecins').query(true).reply(200, [
+      { nom: 'Jacob', address: 'une super adresse 73011' }
+    ]);
+
     const res = await request(app).get('/departements/73/medecins?codePostal=73000');
     expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
   });
 
   test('GET /departements/73/communes/73065 retourne une commune avec population', async () => {
-    const res = await request(app).get('/departements/73/communes/73065');
-    expect(res.statusCode).toBe(200);
-    expect(res.body.population).toBeDefined();
+    nock(base_url_communes)
+      .get('/communes') 
+      .reply(200, [
+        { nom: 'Albertville', code: '73065', population: 10035 },
+      ]);
+
+    const res = await request(app).get('/departements/73/communes');
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body[0].population).toBe(10035);
   });
 
   test('GET /recherche/medecins sans coordonnées retourne une erreur', async () => {
     const res = await request(app).get('/recherche/medecins');
     expect(res.statusCode).toBe(400);
     expect(res.body.message).toBe('Coordonnées invalides');
+  });
+
+  
+  test('GET /recherche/medecins sans coordonnées retourne une erreur', async () => {
+    nock(base_url_localisation)
+      .get('/reverse')
+      .query(true)
+      .reply(200, 
+        { nom: 'Albertville', code: '73065', population: 10035, address : { postcode: "73250" }},
+      );
+
+    nock("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets")
+      .get('/medecins/records')
+      .query(true)
+      .reply(200, {
+      "results" : [{ nom: 'Jacob', address: 'une super adresse 73011', coordonnees : { lon: 6.25, lat: 45.36 } }]
+    });
+
+    const res = await request(app).get('/recherche/medecins?lat=3.025&lon=45.23');
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body[0].nom).toBe("Jacob");
   });
 });
